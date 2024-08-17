38f6775e011944a091821b520b4831793c8e7444
wu-yiqiang <wu_yiqiang@outlook.com>
2024-08-17 14:33:39 +0800

    添加日志功能

--- global/app.go
+++ global/app.go
@@ -1,1 +1,15 @@
 package global
+
+import (
+	"ehse-go/config"
+	"github.com/spf13/viper"
+	"go.uber.org/zap"
+)
+
+type Application struct {
+	ConfigViper *viper.Viper
+	Config      config.Configuration
+	Log         *zap.Logger
+}
+
+var App = new(Application)
--- main.go
+++ main.go
@@ -1,18 +1,27 @@
 package main
 
 import (
+	"ehse-go/bootstrap"
+	"ehse-go/global"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
 func main() {
+	// 初始化配置
+	bootstrap.InitializeConfig()
+
+	// 初始化日志
+	global.App.Log = bootstrap.InitializeLog()
+	global.App.Log.Info("log init success!")
+
 	r := gin.Default()
 
 	// 测试路由
 	r.GET("/ping", func(c *gin.Context) {
-		c.String(http.StatusOK, "xj-demo")
+		c.String(http.StatusOK, "pong")
 	})
 
 	// 启动服务器
-	r.Run(":8080")
+	r.Run(":" + global.App.Config.App.Port)
 }
