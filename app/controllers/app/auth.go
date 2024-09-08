5d0debcb59a7ac78f40525f679cba63d2b39884a
wu-yiqiang <wu_yiqiang@outlook.com>
2024-09-09 00:25:35 +0800

    实现jwt鉴权

---
+++ app/controllers/app/auth.go
@@ -0,0 +1,36 @@
+package app
+
+import (
+	"ehse-go/app/common/request"
+	"ehse-go/app/common/response"
+	"ehse-go/services"
+	"github.com/gin-gonic/gin"
+)
+
+func Login(c *gin.Context) {
+	var form request.Login
+	if err := c.ShouldBindJSON(&form); err != nil {
+		response.ValidateFail(c, request.GetErrorMsg(form, err))
+		return
+	}
+
+	if err, user := services.UserService.Login(form); err != nil {
+		response.BusinessFail(c, err.Error())
+	} else {
+		tokenData, err, _ := services.JwtService.CreateToken(services.AppGuardName, user)
+		if err != nil {
+			response.BusinessFail(c, err.Error())
+			return
+		}
+		response.Success(c, tokenData)
+	}
+}
+
+func Info(c *gin.Context) {
+	err, user := services.UserService.GetUserInfo(c.Keys["id"].(string))
+	if err != nil {
+		response.BusinessFail(c, err.Error())
+		return
+	}
+	response.Success(c, user)
+}
--- routes/api.go
+++ routes/api.go
@@ -2,10 +2,17 @@ package routes
 
 import (
 	"ehse-go/app/controllers/app"
+	"ehse-go/middleware"
+	"ehse-go/services"
 	"github.com/gin-gonic/gin"
 )
 
 // SetApiGroupRoutes 定义 api 分组路由
 func SetApiGroupRoutes(router *gin.RouterGroup) {
 	router.POST("/auth/register", app.Register)
+	router.POST("/auth/login", app.Login)
+	authRouter := router.Group("").Use(middleware.JWTAuth(services.AppGuardName))
+	{
+		authRouter.POST("/auth/info", app.Info)
+	}
 }
